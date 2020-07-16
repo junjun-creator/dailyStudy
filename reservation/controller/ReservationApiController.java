5b97435117e0c96d5a2b42db951cea05581fdca2
BYEONGJUN KIM(JUN) <65852909+junjun-creator@users.noreply.github.com>
2020-07-16 23:00:58 +0900

    RestController 작성중입니다
    RestController 작성중입니다.

--- reservation/controller/ReservationApiController.java
+++ reservation/controller/ReservationApiController.java
@@ -1,39 +1,34 @@
 package kr.or.connect.reservation.controller;
 
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.or.connect.reservation.dto.DisplayInfo;
 import kr.or.connect.reservation.dto.FileInfo;
-import kr.or.connect.reservation.dto.Product;
 import kr.or.connect.reservation.dto.WholeServiceInfo;
 import kr.or.connect.reservation.service.ReservationService;
 
-@Controller
-@RequestMapping(path="/main")
-public class ReservationController {
+@RestController
+@RequestMapping(path="/reservationlists")
+public class ReservationApiController {
 	@Autowired
 	ReservationService reservationService;
 	
 	@GetMapping
-	public String list(@RequestParam(name="start", required=false, defaultValue="0") int start,
-			   ModelMap model) {
+	public Map<String, Object> list(@RequestParam(name="start", required=false, defaultValue="0") int start) {
 		List<FileInfo> list = reservationService.getPromotionImage();
 		//List<Product> productInfo = reservationService.getProductInfo();
 		//List<DisplayInfo> placeName = reservationService.getPlaceName();
@@ -54,39 +49,34 @@ public class ReservationController {
 			pageStartList.add(i * ReservationService.LIMIT);
 		}
 		
-		model.addAttribute("list",list);
+		Map<String, Object> map = new HashMap<>();
+		map.put("list",list);
 		//model.addAttribute("productInfo",productInfo);
 		//model.addAttribute("placeName",placeName);
-		model.addAttribute("productImg",productImg);
-		model.addAttribute("allItem",wholeServiceInfo);
-		model.addAttribute("pageStartList",pageStartList);
-		model.addAttribute("count",count);
-		model.addAttribute("countCategory",countCategory);
-		return "main";
+		map.put("productImg",productImg);
+		map.put("allItem",wholeServiceInfo);
+		map.put("pageStartList",pageStartList);
+		map.put("count",count);
+		map.put("countCategory",countCategory);
+		return map;
 	}
 	
 	@PostMapping//ajax 통신 할것임
-	public @ResponseBody List moreItems(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
-		int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+	public List<WholeServiceInfo> moreItems(@RequestBody int categoryId,@RequestBody int start) throws Exception {
+		int category = categoryId;
 		
 		if(categoryId != 0) {
-			int start = Integer.parseInt(request.getParameter("start"));
-			List<WholeServiceInfo> moreItemsCategory = reservationService.getItemsCategory(categoryId, start);
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("application/json");
+			int startNum = start;
+			List<WholeServiceInfo> moreItemsCategory = reservationService.getItemsCategory(category, startNum);
 			
 			return moreItemsCategory;
 		}
 		else {
-			int start = Integer.parseInt(request.getParameter("start"));
-			List<WholeServiceInfo> moreServiceInfo = reservationService.getAllItems(start);
-			
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("application/json");
+			int startNum = start;
+			List<WholeServiceInfo> moreServiceInfo = reservationService.getAllItems(startNum);
 			
 			return moreServiceInfo;
 		}
 		//if 조건문으로 어떤 list를 전송해 줄것인지 선택하는 로직 작성
 	}
-	
 }
