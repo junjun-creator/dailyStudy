9c4d763cc532b096bbec3968da91ff5381cee076
BYEONGJUN KIM(JUN) <65852909+junjun-creator@users.noreply.github.com>
2020-08-05 23:47:07 +0900

    쿠키와 세션을 통해 상태정보 유지에 관하여 학습하였습니다.
    쿠키와 세션을 통해 상태정보 유지에 관하여 학습하였습니다.

--- guestbook/src/main/java/kr/or/connect/guestbook/service/impl/GuestbookServiceTest.java
+++ guestbook/src/main/java/kr/or/connect/guestbook/service/impl/GuestbookServiceTest.java
@@ -1,6 +1,7 @@
 package kr.or.connect.guestbook.service.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,12 +16,18 @@ public class GuestbookServiceTest {
 		ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 		GuestbookService guestbookService = ac.getBean(GuestbookService.class);
 		
-		Guestbook guestbook = new Guestbook();
-		guestbook.setName("kang kyungmi22");
-		guestbook.setContent("반갑습니다. 여러분. 여러분이 재미있게 공부하고 계셨음 정말 좋겠네요^^22");
-		guestbook.setRegdate(new Date());
-		Guestbook result = guestbookService.addGuestbook(guestbook, "127.0.0.1");
-		System.out.println(result);
+//		Guestbook guestbook = new Guestbook();
+//		guestbook.setName("kang kyungmi22");
+//		guestbook.setContent("반갑습니다. 여러분. 여러분이 재미있게 공부하고 계셨음 정말 좋겠네요^^22");
+//		guestbook.setRegdate(new Date());
+//		Guestbook result = guestbookService.addGuestbook(guestbook, "127.0.0.1");
+//		System.out.println(result);
+		
+		List<Guestbook> list = guestbookService.getGuestbooks(0);
+		for(Guestbook k : list) {
+			System.out.println(k);
+		}
+		
 	}
 
 }
